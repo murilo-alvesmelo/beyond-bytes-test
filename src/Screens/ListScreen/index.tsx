fdf05cae5bcac82fb92b12c6725f9abfbcc7d6e5
Murilo Alves Melo <murilo.melo72@gmail.com>
2024-05-30 00:07:06 -0300

    chore: add img caso quando nao houver endereco

--- src/Screens/ListScreen/index.tsx
+++ src/Screens/ListScreen/index.tsx
@@ -1,7 +1,36 @@
-export default function ListScreen() {
+import Card from "../../Components/Card";
+import styles from "./styles.module.css";
+import undrawDrone from "../../assets/undrawDrone.png";
+import FormType from "../../types";
+
+export default function ListScreen({ enderecos }: { enderecos: FormType[] }) {
   return (
-    <div>
-      <h1>Initial Screen</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Endereços cadastrados</h1>
+      <div className={styles.listContainer}>
+        {enderecos.map((endereco: FormType) => (
+          <Card
+            key={endereco.id}
+            planeta={endereco.planeta}
+            destinatario={endereco.destinatario}
+            contato={endereco.contato}
+            logradouro={endereco.logradouro}
+            bairro={endereco.bairro}
+            cidade={endereco.cidade}
+            uf={endereco.uf}
+            fabrica={endereco.fabrica}
+            lote={endereco.lote}
+            cep={endereco.cep}
+            pais={endereco.pais}
+            estado={endereco.estado}
+          />
+        ))}
+      </div>
+      {enderecos.length === 0 && (
+        <div className={styles.imgContainer}>
+          <img src={undrawDrone} alt="Imagem" className={styles.img} />
+        </div>
+      )}
     </div>
   );
 }
