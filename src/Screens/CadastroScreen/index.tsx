3ceddc50d600e1ff5a040c6d784bc4f8fc52f95e
Murilo Alves Melo <murilo.melo72@gmail.com>
2024-05-30 00:08:04 -0300

    chore: add modal sucess

--- src/Screens/CadastroScreen/index.tsx
+++ src/Screens/CadastroScreen/index.tsx
@@ -3,8 +3,17 @@ import Form from "./Components/Form";
 import InfoCadastro from "./Components/Info";
 import { useState } from "react";
 import FormType from "../../types";
+import Swal from "sweetalert2";
 
-export default function CadastroScreen() {
+type CadastroScreenProps = {
+  enderecos: FormType[];
+  setEnderecos: React.Dispatch<React.SetStateAction<FormType[]>>;
+};
+
+export default function CadastroScreen({
+  enderecos,
+  setEnderecos,
+}: CadastroScreenProps) {
   const [form, setForm] = useState<FormType>({
     destinatario: "",
     contato: "",
@@ -22,13 +31,36 @@ export default function CadastroScreen() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    localStorage.setItem(
-      "enderecos",
-      JSON.stringify({
-        id: crypto.randomUUID(),
-        ...form,
+    const newEndereco = { id: enderecos.length + 1, ...form };
+    setEnderecos([...enderecos, newEndereco]);
+    showSuccessAlert();
+  };
+
+  const showSuccessAlert = () => {
+    Swal.fire({
+      title: "Endereço cadastrado!",
+      text: "Verifique a lista de endereços cadastrados",
+      icon: "success",
+    })
+      .then(() => {
+        setForm({
+          destinatario: "",
+          contato: "",
+          planeta: "",
+          logradouro: "",
+          bairro: "",
+          cep: "",
+          cidade: "",
+          uf: "",
+          pais: "",
+          fabrica: "",
+          lote: "",
+          estado: "",
+        });
       })
-    );
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
